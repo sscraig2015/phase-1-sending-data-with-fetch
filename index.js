91cdb3dc86635b946cd806724cf48083f9d837e2
Sean <craig.sean92@gmail.com>
2021-11-10 13:12:03 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+
+const formData = {
+    name: name,
+    email: email,    
+}
+
+const configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    },
+    body: JSON.stringify(formData)
+}
+
+return fetch("http://localhost:3000/users", configObj)
+.then (function (response) {
+    return response.json();
+})
+.then(function (object) {
+    let body= document.querySelector("body")
+    body.append(object.id)  
+})
+.catch(function(error) {
+    let body = document.querySelector("body")
+    body.append(error.message)
+    
+})
+}
+
